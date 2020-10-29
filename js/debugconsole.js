140a8b9723e2375ab82444bfa833fb487e3595fa
Alex Petrenko <petrenkodesign@gmail.com>
2020-10-29 21:57:51 +0200

    Stable version 1.0.0

--- js/debugconsole.js
+++ js/debugconsole.js
@@ -5,7 +5,7 @@ let debugconsole = {
   console_id: "debug_console",
   console_content_id: "debug_console_content",
 
-  start(this.status) {
+  start(status=this.status) {
     window.onerror = function (msg, url, lnum) {
       debugconsole.log("Error: "+msg+"<br> URL:"+url+"<br> LineNumber:"+lnum, true);
     }
@@ -30,13 +30,13 @@ let debugconsole = {
     if (this.status=="dblog" || this.status=="db")  {}
   },
 
-  log(content, this.error_status) {
+  log(content, thiserror=this.error_status) {
     console.log(content);
     if(typeof content === 'object' && content !== null) {
       content = JSON.stringify(content);
     }
     var errorcolor="";
-    if(this.error_status) errorcolor = 'style="color:'+this.error_color+'"';
+    if(thiserror) errorcolor = 'style="color:'+this.error_color+'"';
     var consoleContent = document.getElementById(console_content_id);
     consoleContent.innerHTML += "<p "+errorcolor+">"+content+"</p>";
     consoleContent.scrollTop = consoleContent.scrollHeight - consoleContent.clientHeight;
