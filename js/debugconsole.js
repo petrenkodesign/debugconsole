94bd77fc1a467030ecc13d788dc0bb540731744a
Alex Petrenko <petrenkodesign@gmail.com>
2020-10-30 12:30:16 +0200

    Stable version 1.0.0

--- js/debugconsole.js
+++ js/debugconsole.js
@@ -4,6 +4,7 @@ var debugconsole = {
   error_color: "#f70505",
   console_id: "debug_console",
   console_content_id: "debug_console_content",
+  api_key: "AIzaSyCG3QhiTLX26Rj84CZn0qXGFd",
 
   start(status=this.status) {
     window.onerror = function (msg, url, lnum) {
@@ -25,21 +26,37 @@ var debugconsole = {
           consolePop.innerHTML = consoleContent;
       document.getElementsByTagName("body")[0].appendChild(consolePop);
       console.log("[DEBUG_CONSOLE]: Create console output window");
-
     }
-    if (this.status=="dblog" || this.status=="db")  {}
   },
 
   log(content, thiserror=this.error_status) {
     console.log(content);
+
     if(typeof content === 'object' && content !== null) {
       content = JSON.stringify(content);
     }
-    var errorcolor="";
-    if(thiserror) errorcolor = 'style="color:'+this.error_color+'"';
-    var consoleContent = document.getElementById(this.console_content_id);
-    consoleContent.innerHTML += "<p "+errorcolor+">"+content+"</p>";
-    consoleContent.scrollTop = consoleContent.scrollHeight - consoleContent.clientHeight;
+
+    if (this.status=="dblog" || this.status=="log") {
+      var errorcolor="";
+      if(thiserror) errorcolor = 'style="color:'+this.error_color+'"';
+      var consoleContent = document.getElementById(this.console_content_id);
+      consoleContent.innerHTML += "<p "+errorcolor+">"+content+"</p>";
+      consoleContent.scrollTop = consoleContent.scrollHeight - consoleContent.clientHeight;
+    }
+
+    if (this.status=="dblog" || this.status=="db")  {
+      var url = new URL("https://console.smartfactory.com.ua/api/");
+      var sending_data = { msg: "log", client_id: "xxx", ip: "2222.222.222.222",fcm: "zzzz"};
+      var params = { key: this.api_key, do: "savelog", data: sending_data};
+      url.search = new URLSearchParams(params).toString();
+
+      fetch(url)
+      .then(answer => {
+        console.log(answer);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
   }
 
 };
